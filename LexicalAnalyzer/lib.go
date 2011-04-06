edfa918faa8fa04bc7f093690f64b9705f46f5f8
Gary Talent <gtalent2@gmail.com>
2011-04-05 23:23:43 -0500

    Finished coding.

--- LexicalAnalyzer/lib.go
+++ LexicalAnalyzer/lib.go
@@ -1,11 +1,24 @@
 package main
 
-import "strings"
+import (
+	"strings"
+)
 
-var symbolTable []string = make([]string, 0)
+var identTable []string = make([]string, 0)
+var numLitTable []string = make([]string, 0)
 var keywords []string = []string{"int", "void", "print", "println", "function", "program", "true", "false",
 	"if", "else", "begin", "end", "while", "return", "boolean", "null"}
 var symbols []string = []string{".", ";", "=", ",", "(", ")", "==", "||", "&&", "!", "/", "*", "-", "+", "<<", ">>", "=<", "=>", "<", ">", "!="}
+
+const (
+	keyword    string = "keyword"
+	identifier = "identifier"
+	symbol     = "symbol"
+	literal    = "literal"
+	comment    = "comment"
+	whitespace = "whitespace"
+	error      = "error"
+)
 
 func match(a, b string) bool {
 	a = strings.ToUpper(a)
@@ -15,6 +28,10 @@ func match(a, b string) bool {
 
 func isCharacter(val byte) bool {
 	return (val < 91 && val > 64) || (val < 123 && val > 96)
+}
+
+func isNumber(val byte) bool {
+	return (47 < val && val < 58)
 }
 
 //Indicates whether or not the given value is a keyword, and if it is, it adjusts for casing.
@@ -30,7 +47,7 @@ func isKeyword(val string) (string, bool) {
 //Indicates whether or not the given value is a symbol.
 func isSymbol(val string, point int) bool {
 	for _, kw := range symbols {
-		if pt2 := point+len(kw); pt2 <= len(val) {
+		if pt2 := point + len(kw); pt2 <= len(val) {
 			if val[point:pt2] == kw {
 				return true
 			}
@@ -39,20 +56,67 @@ func isSymbol(val string, point int) bool {
 	return false
 }
 
+func isComment(val string, point int) bool {
+	return point+1 < len(val) && val[point] == '/' && val[point+1] == '/'
+}
+
 func isWhitespace(val byte) bool {
 	return val == ' ' || val == '\t' || val == '\n'
 }
 
-func parseToken(val string, start int) (string, int) {
-	token := ""
-	point := start
-
-	if isCharacter(val[start]) {
-		//is a keyword or identifier
-		for ; !isSymbol(val, point) && !isWhitespace(val[point]); point++ {
-			token += string(val[point])
+func getSymbol(val string, point int) string {
+	for _, kw := range symbols {
+		if pt2 := point + len(kw); pt2 <= len(val) {
+			if val[point:pt2] == kw {
+				return kw
+			}
 		}
 	}
+	return ""
+}
 
-	return token, point
+//Returns: the token type, the token, the point in the file where the tokenizer left off
+func nextToken(val string, point int) (string, string, int) {
+	switch {
+	case isWhitespace(val[point]):
+		return whitespace, string(val[point]), point + 1
+	case isCharacter(val[point]): //is a keyword or identifier
+		token := ""
+		for !isSymbol(val, point) && !isWhitespace(val[point]) {
+			token += string(val[point])
+			point++
+		}
+		if kw, b := isKeyword(token); b { //is keyword
+			return keyword, kw, point
+		}
+		//is identifier
+		identTable = append(identTable, token)
+		return identifier, token, point
+	case isComment(val, point):
+		token := ""
+		for ; val[point] != '\n'; point++ {
+			token += string(val[point])
+		}
+		return comment, token, point
+	case isSymbol(val, point):
+		s := getSymbol(val, point)
+		return symbol, s, point + len(s)
+	default: //is a literal
+		token := ""
+		if isNumber(val[point]) { //is a number literal
+			for ; isNumber(val[point]); point++ {
+				token += string(val[point])
+			}
+			numLitTable = append(numLitTable, token)
+		} else {
+			point++
+			for ; val[point] != '"'; point++ {
+				token += string(val[point])
+			}
+			point++
+		}
+		return literal, token, point
+	}
+
+	return error, string(val[point]), point
 }
--- LexicalAnalyzer/main.go
+++ LexicalAnalyzer/main.go
@@ -3,26 +3,56 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 )
 
 func main() {
 	flag.Parse()
 	args := flag.Args()
-	for i, a := range args {
-		fmt.Println(i, a)
+	for _, a := range args {
+		file, err := ioutil.ReadFile(a)
+		if err != nil {
+			fmt.Println(err.String())
+			continue
+		}
+		val := string(file)
+		point := 0
+		var tokenType string
+		for point < len(val) {
+			tokenType, _, point = nextToken(val, point)
+			fmt.Print(tokenType, ", ")
+		}
 	}
+	/*
+		test1()
+		test2()
+		test3()
+	*/
 }
 
+//Tests symbol identifier.
 func test1() {
 	fmt.Println("Test 1:")
-	fmt.Println("\t", !isSymbol("rri", 0))
-	fmt.Println("\t", isSymbol(">=", 0))
-	fmt.Println("\t", isSymbol("!=", 0))
+	fmt.Println("\tGood =", !isSymbol("rri", 0))
+	fmt.Println("\tGood =", isSymbol(">=", 0))
+	fmt.Println("\tGood =", isSymbol("!=", 0))
 }
 
+//Tests keyword identifier.
 func test2() {
 	fmt.Println("Test 2:")
-	fmt.Println("\t", isWhitespace(' '))
-	fmt.Println("\t", isWhitespace('\n'))
-	fmt.Println("\t", isWhitespace('\t'))
+	_, ok := isKeyword("function")
+	fmt.Println("\tGood =", ok)
+	_, ok = isKeyword("FUNCTION")
+	fmt.Println("\tGood =", ok)
+	_, ok = isKeyword("narf")
+	fmt.Println("\tGood =", !ok)
+}
+
+//Tests whitespace identifier.
+func test3() {
+	fmt.Println("Test 3:")
+	fmt.Println("\tGood =", isWhitespace(' '))
+	fmt.Println("\tGood =", isWhitespace('\n'))
+	fmt.Println("\tGood =", isWhitespace('\t'))
 }
